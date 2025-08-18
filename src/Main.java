94bc14c74e91c54dda341d119dd82594de59ab51
oyasumip <taoqibao_halo@163.com>
2025-08-18 09:56:41 +0800

    第0步完成

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+
+        String name = "oyasumip";
+        int age = 16;
+        double height = 1.81;
+        boolean isStudent = true;
+
+
+        System.out.println("Name:" + name);
+        System.out.println("Age:" + age);
+        System.out.println("Height:" + height);
+        System.out.println("isStudent:" + isStudent);
+        //第0步完成
+
     }
 }
